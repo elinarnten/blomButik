daf1b38dd16172291468af156b639bf552d808bf
elinarnten <elin.arnten@hotmail.com>
2022-03-08 17:13:09 +0100

    added cards and icons

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import "../App.css";
 import Header from "./Header";
 import Footer from "./Footer";
+import Contact from "./Contact";
 
 function App() {
   return (
     <>
       <Header />
-      <div className="App">
-        <h1>hello world</h1>
-      </div>
+      <Contact />
+      <div className="App"></div>
       <Footer />
     </>
   );
--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,5 +1,26 @@
+import { Box, Card, CardMedia } from "@mui/material";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import PhoneIcon from "@mui/icons-material/Phone";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import AlternateEmailIcon from "@mui/icons-material/AlternateEmail";
+
 function Contact() {
-  return;
+  return (
+    <Box sx={{ display: "flex" }}>
+      <Card>
+        <LocationOnIcon style={{ color: "#84A86F" }} />
+      </Card>
+      <Card>
+        <AlternateEmailIcon style={{ color: "#84A86F" }} />
+      </Card>
+      <Card>
+        <PhoneIcon style={{ color: "#84A86F" }} />
+      </Card>
+      <Card>
+        <InstagramIcon style={{ color: "#84A86F" }} />
+      </Card>
+    </Box>
+  );
 }
 
 export default Contact;
