70c70ef1b1c09d3eae6fff8aa4d263b489cb9b0f
elinarnten <elin.arnten@hotmail.com>
2022-03-09 11:38:44 +0100

    styling

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -9,79 +9,96 @@ import Image from "../assets/flowerBackground.png";
 
 function Contact() {
   return (
-    <Box
-      sx={{
-        display: "flex",
-        justifyContent: "space-around",
-        alignItems: "center",
-        my: "3rem",
-        //height: "100vh",
+    <div
+      style={{
+        backgroundImage:
+          "url(../assets/flowerBackground.png) no-repeat center center fixed",
       }}
     >
-      <Card style={{ backgroundColor: "#F4EAC6", height: 180, width: 180 }}>
-        <div
-          style={{
-            alignItems: "center",
-            justifyContent: "center",
-            display: "flex",
-            padding: 15,
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-around",
+          alignItems: "center",
+          my: "3rem",
+          background:
+            "url(../assets/flowerBackground.png) no-repeat center center fixed",
+          //height: "100vh",
+        }}
+      >
+        <Card style={{ backgroundColor: "#F4EAC6", height: 180, width: 180 }}>
+          <div
+            style={{
+              alignItems: "center",
+              justifyContent: "center",
+              display: "flex",
+              padding: 15,
+            }}
+          >
+            <LocationOnIcon style={{ color: "#84A86F", fontSize: "4rem" }} />
+          </div>
+
+          <Typography sx={{ mx: "auto", textAlign: "center" }}>
+            Anders Personsgatan 18 <br></br> 416 64 Göteborg
+          </Typography>
+        </Card>
+
+        <Card style={{ backgroundColor: "#F4EAC6", height: 180, width: 180 }}>
+          <div
+            style={{
+              alignItems: "center",
+              justifyContent: "center",
+              display: "flex",
+              padding: 15,
+            }}
+          >
+            <AlternateEmailIcon
+              style={{ color: "#84A86F", fontSize: "4rem" }}
+            />
+          </div>
+          <Typography sx={{ mx: "auto", textAlign: "center" }}>
+            info@bb.se
+          </Typography>
+        </Card>
+
+        <Card
+          sx={{
+            backgroundColor: "rgba(244, 234, 198, 0.7 )",
+            height: 180,
+            width: 180,
           }}
         >
-          <LocationOnIcon style={{ color: "#84A86F", fontSize: "4rem" }} />
-        </div>
+          <div
+            style={{
+              alignItems: "center",
+              justifyContent: "center",
+              display: "flex",
+              padding: 15,
+            }}
+          >
+            <PhoneIcon style={{ color: "#84A86F", fontSize: "4rem" }} />
+          </div>
+          <Typography sx={{ mx: "auto", textAlign: "center" }}>
+            0712345678
+          </Typography>
+        </Card>
 
-        <Typography sx={{ mx: "auto", textAlign: "center" }}>
-          Anders Personsgatan 18 <br></br> 416 64 Göteborg
-        </Typography>
-      </Card>
+        <Card style={{ backgroundColor: "#F4EAC6", height: 180, width: 180 }}>
+          <div
+            style={{
+              alignItems: "center",
+              justifyContent: "center",
+              display: "flex",
+              padding: 15,
+            }}
+          >
+            <InstagramIcon style={{ color: "#84A86F", fontSize: "4rem" }} />
+          </div>
 
-      <Card style={{ backgroundColor: "#F4EAC6", height: 180, width: 180 }}>
-        <div
-          style={{
-            alignItems: "center",
-            justifyContent: "center",
-            display: "flex",
-            padding: 15,
-          }}
-        >
-          <AlternateEmailIcon style={{ color: "#84A86F", fontSize: "4rem" }} />
-        </div>
-        <Typography sx={{ mx: "auto", textAlign: "center" }}>
-          info@bb.se
-        </Typography>
-      </Card>
-
-      <Card sx={{ backgroundColor: "#F4EAC6", height: 180, width: 180 }}>
-        <div
-          style={{
-            alignItems: "center",
-            justifyContent: "center",
-            display: "flex",
-            padding: 15,
-          }}
-        >
-          <PhoneIcon style={{ color: "#84A86F", fontSize: "4rem" }} />
-        </div>
-        <Typography sx={{ mx: "auto", textAlign: "center" }}>
-          0712345678
-        </Typography>
-      </Card>
-
-      <Card style={{ backgroundColor: "#F4EAC6", height: 180, width: 180 }}>
-        <div
-          style={{
-            alignItems: "center",
-            justifyContent: "center",
-            display: "flex",
-            padding: 15,
-          }}
-        >
-          <InstagramIcon style={{ color: "#84A86F", fontSize: "4rem" }} />
-        </div>
-
-        <Typography sx={{ mx: "auto", textAlign: "center" }}></Typography>
-      </Card>
-    </Box>
+          <Typography sx={{ mx: "auto", textAlign: "center" }}></Typography>
+        </Card>
+      </Box>
+    </div>
   );
 }
 export default Contact;
