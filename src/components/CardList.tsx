e26453d484edd6121107ba492a625679d71d29e9
elinarnten <elin.arnten@hotmail.com>
2022-03-10 17:53:29 +0100

    removed comment code

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -10,13 +10,16 @@ interface shopItems {
 export function Cardlist(shopItems: shopItems[]) {
   return (
     <>
-      {/* { shopItems.map((item,i) =>{
-               return (
-                <CardView key={item} img={shopItems[i].img} title={shopItems[i].title} price={shopItems[i].price}/>
-               )
-
-           })
-       } */}
+      {shopItems.map((item, i) => {
+        return (
+          <CardView
+            key={item}
+            img={shopItems[i].img}
+            title={shopItems[i].title}
+            price={shopItems[i].price}
+          />
+        );
+      })}
     </>
   );
 }
