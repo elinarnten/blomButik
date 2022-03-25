77347d7752f72d526723698ac56fa5c6031b5514
elinarnten <elin.arnten@hotmail.com>
2022-03-25 11:06:47 +0100

    changed the footer to mui components

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,44 +1,24 @@
 import { CSSProperties } from "react";
 import Edit from "@mui/icons-material/Edit";
+import { BottomNavigation, BottomNavigationAction, Box } from "@mui/material";
+import EditIcon from "@mui/icons-material/Edit";
+import { Link } from "react-router-dom";
 
 function Footer() {
   return (
-    <footer style={footStyle}>
-      <div style={footContainer}>
-        <div style={footHolder}>
-          <p>Kontakt</p>
-          <p>Om oss</p>
-          <p>Leverans</p>
-        </div>
-        <Edit style={{ display: "flex", justifyContent: "flex-end" }} />
-      </div>
-    </footer>
+    <Box sx={{ color: "black" }}>
+      <BottomNavigation showLabels>
+        <BottomNavigationAction label="Kontakt" />
+
+        <BottomNavigationAction label="Om oss" />
+
+        <BottomNavigationAction
+          label="Redigera"
+          icon={<EditIcon fontSize="small" />}
+        />
+      </BottomNavigation>
+    </Box>
   );
 }
-
-const footStyle: CSSProperties = {
-  display: "flex",
-  bottom: 0,
-  width: "100%",
-  height: "4rem",
-  marginTop: "1rem",
-};
-
-const footContainer: CSSProperties = {
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
-  borderTop: "black solid 1px",
-  width: "100%",
-  marginRight: "4rem",
-  marginLeft: "4rem",
-};
-
-const footHolder: CSSProperties = {
-  margin: "1rem",
-  display: "flex",
-  justifyContent: "space-around",
-  width: "100%",
-};
 
 export default Footer;
